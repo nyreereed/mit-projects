041d7ebdf9234dd8c958bb9c0f0234daad2b6bbd
nyreereed <83997118+nyreereed@users.noreply.github.com>
2021-06-23 23:25:58 -0500

    Add files via upload

---
+++ Week-5 Assignments/Walk Boston Exercise/walkboston.js
@@ -0,0 +1,54 @@
+function renderPosts(boston, container) {
+  const people = boston.data;
+  const len = boston.data.length;
+  let html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="post">' + '<h2>' + people[i][8] + '</h2>' + '<h3>' + people[i][11] + '</h3>';
+  }
+   container.innerHTML = '<ul id = "data">' + html + '</ul>';
+}
+
+function renderTopSalaries(boston, container) {
+  
+  var people = boston.data;
+  var topSalaries = people.sort((a, b) => b[11] - a[11]).slice(0, 5);
+  const len = topSalaries.length;
+  var html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="top">' +
+      '<h2>' +
+      topSalaries[i][8] +
+      '</h2>' +
+      '<h3>' +
+      topSalaries[i][11] +
+      '</h3>';
+  }
+
+  container.innerHTML = '<ul id = "topSalaries">' + html + '</ul>';
+}
+
+  // TODO: add code to display the html variable inside a ul element with id="data"
+  
+renderPosts(boston, document.getElementById('container'));
+
+function renderTopEmployees(boston, container) {
+  
+  var people = boston.data;
+  var topEmployees = people.filter((a) => a[11] > 200000);
+  const len = topEmployees.length;
+  var html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="top">' +
+      '<h2>' +
+      topEmployees[i][8] +
+      '</h2>' +
+      '<h3>' +
+      topEmployees[i][11] +
+      '</h3>';
+  }
+
+  container.innerHTML += '<ul id = "topEmployees">' + html + '</ul>';
+}
